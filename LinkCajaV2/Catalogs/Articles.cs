9f3912155b7c4de5212a43c4e65c32e0b504c9f5
Miguel Angel <mikuymegumi@gmail.com>
2026-05-07 16:56:38 -0600

    ya quedo anidado la busqueda a la impresion

--- LinkCajaV2/Catalogs/Articles.cs
+++ LinkCajaV2/Catalogs/Articles.cs
@@ -1,4 +1,5 @@
 ﻿using LinkCajaV2.Data;
+using LinkCajaV2.Model;
 using QuestPDF.Fluent;
 using QuestPDF.Helpers;
 using QuestPDF.Infrastructure;
@@ -7,6 +8,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Forms;
 
 namespace LinkCajaV2.Catalogs
@@ -17,12 +19,14 @@ namespace LinkCajaV2.Catalogs
         public bool IsVenta { get; set; } = false;
         public bool IsReceta { get; set; } = false;
         public int IdSeleccionado { get; set; }
+        private List<ListArticlesModel> ListaImprimir { get; set; }
+        public bool Impresion = false;
         public Articles()
         {
             InitializeComponent();
         }
 
-        private void BtnBuscar_Click(object sender, EventArgs e)
+        private async void BtnBuscar_Click(object sender, EventArgs e)
         {
             if (txtNombre.Text.Trim() == "" && txtCodigo.Text.Trim() == "")
             {
@@ -30,25 +34,35 @@ namespace LinkCajaV2.Catalogs
                 if (resultado == DialogResult.No)
                 {
                     return;
-                }
+                }       
             }
-            BuscarArticulos();
+            Impresion = false;
+           await BuscarArticulos();
         }
-        public async void BuscarArticulos() {
+        public async Task BuscarArticulos()
+        {
             progressBar1.Style = ProgressBarStyle.Marquee; // La barra empieza a moverse sola
             progressBar1.MarqueeAnimationSpeed = 30; // Velocidad de la animación
             btnNuevo.Enabled = false;
             BtnBuscar.Enabled = false;
-            dgvArticulos.DataSource = null;
-            dgvArticulos.Columns.Clear();
+            BtnImpresion2.Enabled = false;
+            if (Impresion == false)
+            {
+                dgvArticulos.DataSource = null;
+                dgvArticulos.Columns.Clear();
+            }
+         
             try
             {
                 AppRepository obj = new AppRepository();
-                var lista = await Task.Run(() => IsVenta==false?             
+                var lista = await Task.Run(() => IsVenta == false ?
                 obj.GetArticles(txtCodigo.Text, txtNombre.Text, IsReceta) :
                 obj.GetArticlesActives(txtCodigo.Text, txtNombre.Text)
                 );
-                dgvArticulos.DataSource = lista != null && lista.Count > 0 ? lista : null;
+                if (Impresion == false)
+                    dgvArticulos.DataSource = lista != null && lista.Count > 0 ? lista : null;
+                else
+                    ListaImprimir = lista;
                 if (lista == null || lista.Count == 0)
                 {
                     MessageBox.Show("No se encontraron articulos.", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
@@ -56,8 +70,11 @@ namespace LinkCajaV2.Catalogs
                 }
                 else
                 {
-                    AgregarBotones();
-                    MessageBox.Show("Carga completa", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    if (Impresion == false)
+                    {
+                        AgregarBotones();
+                        MessageBox.Show("Carga completa", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }                  
                 }
             }
             catch (Exception ex)
@@ -70,15 +87,16 @@ namespace LinkCajaV2.Catalogs
                 progressBar1.Value = 0;
                 btnNuevo.Enabled = true;
                 BtnBuscar.Enabled = true;
+                BtnImpresion2.Enabled = true;
             }
         }
 
-        private void btnNuevo_Click(object sender, EventArgs e)
+        private async void btnNuevo_Click(object sender, EventArgs e)
         {
             Article m = new Article();
             m.Id = 0;
             m.ShowDialog();
-            BuscarArticulos();
+            await BuscarArticulos();
         }
 
         private async void dgvArticulos_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -93,7 +111,7 @@ namespace LinkCajaV2.Catalogs
                     Article m = new Article();
                     m.Id = Convert.ToInt32(Id);
                     m.ShowDialog();
-                    BuscarArticulos();
+                    await BuscarArticulos();
                     break;
                 case "btnAsignar":
                     this.IdSeleccionado = Convert.ToInt32(Id);
@@ -103,20 +121,20 @@ namespace LinkCajaV2.Catalogs
                 case "btnCambiar":
                     AppRepository obj = new AppRepository();
                     await obj.UpdateStatusArticle(Convert.ToInt32(Id));
-                    BuscarArticulos();
+                    await BuscarArticulos();
                     break;
                 case "btnStock":
                     Stock s = new Stock();
                     s.IdArticle = Convert.ToInt32(Id);
                     s.Nombre = dgvArticulos.Rows[e.RowIndex].Cells["Articulo"].Value.ToString();
                     s.ShowDialog();
-                    BuscarArticulos();
+                    await BuscarArticulos();
                     break;
                 case "btnProveedores":
                     PricesSuppliers pr = new PricesSuppliers();
                     pr.IdArticle = Convert.ToInt32(Id);
                     pr.ShowDialog();
-                    BuscarArticulos();
+                    await BuscarArticulos();
                     break;
             }
         }
@@ -170,7 +188,7 @@ namespace LinkCajaV2.Catalogs
         private async void btnImprimir_Click(object sender, EventArgs e)
         {
             AppRepository obj = new AppRepository();
-            var lista = await Task.Run(() => 
+            var lista = await Task.Run(() =>
               obj.GetArticles(txtCodigo.Text, txtNombre.Text, IsReceta)
               );
             try
@@ -182,21 +200,26 @@ namespace LinkCajaV2.Catalogs
                 string rutaCompleta = Path.Combine(AppDomain.CurrentDomain.BaseDirectory + "\\Impresiones", nombreArchivo);
 
                 // 2. Crear el documento
-                var documento = Document.Create(container => {
-                    container.Page(page => {
+                var documento = Document.Create(container =>
+                {
+                    container.Page(page =>
+                    {
                         page.Size(PageSizes.A4);
                         page.Margin(1, Unit.Centimetre);
                         page.PageColor(Colors.White);
 
-                        page.Content().Table(table => {
-                            table.ColumnsDefinition(columns => {
+                        page.Content().Table(table =>
+                        {
+                            table.ColumnsDefinition(columns =>
+                            {
                                 columns.RelativeColumn(); //Ancho fijo ConstantColumn(80) o relativo RelativeColumn()
                                 columns.RelativeColumn();
                                 columns.RelativeColumn();
                                 columns.RelativeColumn();
                             });
 
-                            table.Header(header => {
+                            table.Header(header =>
+                            {
                                 header.Cell().Element(CellStyle).Text("Código");
                                 header.Cell().Element(CellStyle).Text("Nombre");
                                 header.Cell().Element(CellStyle).Text("Por cada");
@@ -256,15 +279,19 @@ namespace LinkCajaV2.Catalogs
 
         private async void BtnImpresion2_Click(object sender, EventArgs e)
         {
+            if (txtNombre.Text.Trim() == "" && txtCodigo.Text.Trim() == "")
+            {
+                DialogResult resultado = MessageBox.Show("Ha dejado el campo vacio, esto buscara a todos los articulos pero puede demorar ¿Quiere continuar?", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (resultado == DialogResult.No)
+                {
+                    return;
+                }
+            }
+            Impresion = true;
+            await BuscarArticulos();
             try
             {
-                // 1. Obtener la lista de artículos (usando tu lógica actual)
-                AppRepository obj = new AppRepository();
-                var lista = await System.Threading.Tasks.Task.Run(() => 
-                    obj.GetArticles(txtCodigo.Text, txtNombre.Text, IsReceta) 
-                );
-
-                if (lista == null || lista.Count == 0)
+                if (ListaImprimir == null || ListaImprimir.Count == 0)
                 {
                     MessageBox.Show("No hay artículos para mostrar en el PDF.");
                     return;
@@ -300,7 +327,7 @@ namespace LinkCajaV2.Catalogs
                             inlined.Spacing(8); // Espacio entre recuadros
                             inlined.AlignCenter(); // Centra la cuadrícula en la hoja
 
-                            foreach (var item in lista)
+                            foreach (var item in ListaImprimir)
                             {
                                 inlined.Item().Element(c => DibujarCuadroArticulo(c, item.Articulo, item.Precio));
                             }
