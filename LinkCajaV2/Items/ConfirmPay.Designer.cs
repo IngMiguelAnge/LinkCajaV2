c1b8bf1431ab410973453840b3618692ecb98135
Miguel Angel <mikuymegumi@gmail.com>
2026-05-22 01:26:01 -0600

    diseño confirmar pago listo

--- LinkCajaV2/Items/ConfirmPay.Designer.cs
+++ LinkCajaV2/Items/ConfirmPay.Designer.cs
@@ -34,54 +34,61 @@
             this.btnConfirmar = new System.Windows.Forms.Button();
             this.lblCambio = new System.Windows.Forms.Label();
             this.btnCancelar = new System.Windows.Forms.Button();
+            this.lblTitulo = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.nudRecibido)).BeginInit();
             this.SuspendLayout();
             // 
             // lblTotal
             // 
             this.lblTotal.AutoSize = true;
-            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTotal.Location = new System.Drawing.Point(38, 27);
+            this.lblTotal.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(117)))), ((int)(((byte)(125)))));
+            this.lblTotal.Location = new System.Drawing.Point(40, 100);
             this.lblTotal.Name = "lblTotal";
-            this.lblTotal.Size = new System.Drawing.Size(293, 82);
-            this.lblTotal.TabIndex = 0;
+            this.lblTotal.Size = new System.Drawing.Size(108, 38);
+            this.lblTotal.TabIndex = 3;
             this.lblTotal.Text = "TOTAL:";
             // 
             // lblRecibido
             // 
             this.lblRecibido.AutoSize = true;
-            this.lblRecibido.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecibido.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecibido.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(117)))), ((int)(((byte)(125)))));
             this.lblRecibido.Location = new System.Drawing.Point(38, 149);
             this.lblRecibido.Name = "lblRecibido";
-            this.lblRecibido.Size = new System.Drawing.Size(412, 82);
-            this.lblRecibido.TabIndex = 2;
-            this.lblRecibido.Text = "RECIBIDO:";
+            this.lblRecibido.Size = new System.Drawing.Size(204, 28);
+            this.lblRecibido.TabIndex = 4;
+            this.lblRecibido.Text = "EFECTIVO RECIBIDO:";
             // 
             // nudRecibido
             // 
             this.nudRecibido.DecimalPlaces = 2;
-            this.nudRecibido.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nudRecibido.Location = new System.Drawing.Point(444, 149);
+            this.nudRecibido.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudRecibido.Location = new System.Drawing.Point(40, 185);
             this.nudRecibido.Maximum = new decimal(new int[] {
             100000,
             0,
             0,
             0});
             this.nudRecibido.Name = "nudRecibido";
-            this.nudRecibido.Size = new System.Drawing.Size(380, 89);
-            this.nudRecibido.TabIndex = 3;
+            this.nudRecibido.Size = new System.Drawing.Size(200, 55);
+            this.nudRecibido.TabIndex = 0;
+            this.nudRecibido.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             this.nudRecibido.KeyDown += new System.Windows.Forms.KeyEventHandler(this.nudRecibido_KeyDown);
             this.nudRecibido.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.nudRecibido_KeyPress);
             this.nudRecibido.KeyUp += new System.Windows.Forms.KeyEventHandler(this.nudRecibido_KeyUp);
             // 
             // btnConfirmar
             // 
-            this.btnConfirmar.BackColor = System.Drawing.Color.Lime;
-            this.btnConfirmar.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnConfirmar.Location = new System.Drawing.Point(145, 380);
+            this.btnConfirmar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(146)))), ((int)(((byte)(189)))), ((int)(((byte)(58)))));
+            this.btnConfirmar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnConfirmar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConfirmar.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfirmar.ForeColor = System.Drawing.Color.White;
+            this.btnConfirmar.Location = new System.Drawing.Point(40, 280);
             this.btnConfirmar.Name = "btnConfirmar";
-            this.btnConfirmar.Size = new System.Drawing.Size(511, 98);
-            this.btnConfirmar.TabIndex = 4;
+            this.btnConfirmar.Size = new System.Drawing.Size(240, 45);
+            this.btnConfirmar.TabIndex = 1;
             this.btnConfirmar.Text = "CONFIRMAR";
             this.btnConfirmar.UseVisualStyleBackColor = false;
             this.btnConfirmar.Click += new System.EventHandler(this.btnConfirmar_Click);
@@ -89,30 +96,50 @@
             // lblCambio
             // 
             this.lblCambio.AutoSize = true;
-            this.lblCambio.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCambio.Location = new System.Drawing.Point(48, 262);
+            this.lblCambio.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCambio.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(110)))), ((int)(((byte)(203)))));
+            this.lblCambio.Location = new System.Drawing.Point(270, 170);
             this.lblCambio.Name = "lblCambio";
-            this.lblCambio.Size = new System.Drawing.Size(346, 82);
+            this.lblCambio.Size = new System.Drawing.Size(349, 96);
             this.lblCambio.TabIndex = 5;
             this.lblCambio.Text = "CAMBIO:";
             // 
             // btnCancelar
             // 
-            this.btnCancelar.BackColor = System.Drawing.Color.Red;
-            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancelar.Location = new System.Drawing.Point(145, 555);
+            this.btnCancelar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancelar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(117)))), ((int)(((byte)(125)))));
+            this.btnCancelar.Location = new System.Drawing.Point(295, 280);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(511, 98);
-            this.btnCancelar.TabIndex = 6;
+            this.btnCancelar.Size = new System.Drawing.Size(240, 45);
+            this.btnCancelar.TabIndex = 2;
             this.btnCancelar.Text = "CANCELAR";
             this.btnCancelar.UseVisualStyleBackColor = false;
             this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(110)))), ((int)(((byte)(203)))));
+            this.lblTitulo.Location = new System.Drawing.Point(33, 23);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(259, 38);
+            this.lblTitulo.TabIndex = 25;
+            this.lblTitulo.Text = "FINALIZAR VENTA";
+            // 
             // ConfirmPay
             // 
+            this.AcceptButton = this.btnConfirmar;
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(885, 689);
+            this.BackColor = System.Drawing.Color.White;
+            this.CancelButton = this.btnCancelar;
+            this.ClientSize = new System.Drawing.Size(916, 334);
+            this.Controls.Add(this.lblTitulo);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.lblCambio);
             this.Controls.Add(this.btnConfirmar);
@@ -122,8 +149,8 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "ConfirmPay";
+            this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Confirmación";
             this.Load += new System.EventHandler(this.ConfirmPay_Load);
             ((System.ComponentModel.ISupportInitialize)(this.nudRecibido)).EndInit();
             this.ResumeLayout(false);
@@ -139,5 +166,6 @@
         private System.Windows.Forms.Button btnConfirmar;
         private System.Windows.Forms.Label lblCambio;
         private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label lblTitulo;
     }
 }
